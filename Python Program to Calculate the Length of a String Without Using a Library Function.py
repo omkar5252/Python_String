71fba1663a27cf594402bec218b47bee308e357d
Omkar Dorugade <100912340+omkar5252@users.noreply.github.com>
2022-05-02 17:10:06 +0530

    Update and rename 7.py to Python Program to Calculate the Length of a String Without Using a Library Function.py

---
+++ Python Program to Calculate the Length of a String Without Using a Library Function.py
@@ -0,0 +1,8 @@
+# Python Program to Calculate the Length of a String Without Using a Library Function
+
+str = input("Enter a string:")
+count = 0
+for x in str:
+    count += 1
+print("Length of string:")
+print(count)
